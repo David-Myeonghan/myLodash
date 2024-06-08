12ab0bb813ec33805348ad831bf2c95de26f0103
davidRyu <david.ryu@imagoworks.ai>
2024-06-09 02:12:28 +0900

    feat: find implementation

--- src/find.ts
+++ src/find.ts
@@ -1,8 +1,8 @@
-type collectionType = unknown;
-type predicateType = unknown;
-
-export default function find(collection: collectionType, predicate: predicateType ) {
-    console.log(predicate);
-
-    return collection;
+export default function find<T>(collection: T[], predicate: (item: T) => boolean ): T | undefined {
+    for (let element of collection) {
+        if (predicate(element)) {
+            return element;
+        }
+    }
+    return undefined;
 }
