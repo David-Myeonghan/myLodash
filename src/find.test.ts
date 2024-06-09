6ef3bb30b394a48556a6007b363a4f295a785ec9
davidRyu <david.ryu@imagoworks.ai>
2024-06-10 08:21:32 +0900

    feat: find 구현 및 테스트 코드

--- src/find.test.ts
+++ src/find.test.ts
@@ -15,16 +15,34 @@ const users: User[] = [
 describe('find function', () => {
     test('finds item with a matching predicate function', () => {
         const result = find(users, o => o.age === 1);
-        expect(result).toEqual(users[2]);
+        expect(result).toEqual({ user: 'pebbles', age: 1, active: true });
     });
 
-    // test('finds item with a matching predicate object', () => {
-    //     const result = find(users, {age: 36, active: true});
-    //     expect(result).toEqual(users[0])
-    // })
+    test('finds item with a matching predicate object', () => {
+        const result = find(users, { age: 36, active: true });
+        expect(result).toEqual({ user: 'barney', age: 36, active: true });
+    });
 
-    // test('finds item with a matching predicate array', () => {
-    //     const result = find(users, ['acitve', false]);
-    //     expect(result).toEqual(users[1])
-    // })
-})
+    test('finds item with a matching predicate array', () => {
+        const result = find(users, ['active', false]);
+        expect(result).toEqual({ user: 'fred', age: 40, active: false });
+    });
+
+    test('finds item with a matching predicate string', () => {
+        const result = find(users, 'active');
+        expect(result).toEqual({ user: 'barney', age: 36, active: true });
+    });
+
+    test('returns undefined if no matching item is found', () => {
+        const result = find(users, o => o.age === 100);
+        expect(result).toBeUndefined();
+    });
+
+    test('works with different data types in the array', () => {
+        const mixedArray = [1, 'two', { a: 3 }, true];
+        expect(find(mixedArray, item => typeof item === 'string')).toBe('two');
+        expect(find(mixedArray, item => typeof item === 'number')).toBe(1);
+        expect(find(mixedArray, { a: 3 })).toEqual({ a: 3 });
+        expect(find(mixedArray, item => item === true)).toBe(true);
+    });
+});
--- src/find.ts
+++ src/find.ts
@@ -1,8 +1,42 @@
-export default function find<T>(collection: T[], predicate: (item: T) => boolean ): T | undefined {
-    for (let element of collection) {
-        if (predicate(element)) {
-            return element;
+type Predicate<T> = (item: T) => boolean | Partial<T> | [keyof T, unknown] | keyof T;
+export default function find<T>(collection: T[] | {[key: string]: T}, predicate: Predicate<T>): T | undefined {
+    const iterateeFunc = iteratee(predicate);
+
+    if (Array.isArray(collection)) {
+        for (const element of collection) {
+            if (iterateeFunc(element)) {
+                return element;
+            }
+        }
+    } else {
+        for (const key in collection) {
+            if (iterateeFunc(collection[key])) {
+                return collection[key];
+            }
         }
     }
     return undefined;
+}
+
+function iteratee<T>(predicate: Predicate<T>): (item: T) => boolean {
+    if (typeof predicate === 'function') {
+        return predicate as (item: T) => boolean;
+    } else if (typeof predicate === 'object' && !Array.isArray(predicate)) {
+        return (item: T) => isMatch(item, predicate as Partial<T>);
+    } else if (Array.isArray(predicate)) {
+        const [key, value] = predicate as [keyof T, unknown];
+        return (item: T) => item[key] === value;
+    } else if (typeof predicate === 'string') {
+        return (item: T) => Boolean(item[predicate as keyof T]);
+    }
+    return (_: T) => false;
+}
+
+function isMatch<T>(item: T, predicate: Partial<T>) :boolean {
+    for (const key in predicate) {
+        if (predicate[key] !== item[key]) {
+            return false;
+        }
+    }
+    return true;
 }
